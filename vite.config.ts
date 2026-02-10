72d3cbd1ba1ca08c25248f3cae3e36897c3c7cfa
Vedant Maslekar <watchingvideosandstorage01@gmail.com>
2026-02-10 14:05:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export defalt defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
-        port: 3000,
+        port: 3000,u
         host: '0.0.0.0',
       },
       plugins: [react()],
